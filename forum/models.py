20ccbf590e55a8b46091616c5e1e28d11c59b010
Alexander Bladh <bladh.alexander@gmail.com>
2020-10-26 20:04:30 +0100

    Some refactoring

---
+++ forum/models.py
@@ -0,0 +1,53 @@
+threads = {}
+users = {}
+
+
+def create_user(username, email, password):
+    if email in users.keys():
+        print(email + " has already been registered")
+        return
+    user = {"username": username, "email": email, "password": password}
+    users[email] = user
+
+
+def create_post(poster, contents):
+    post = {"poster": poster, "content": contents}
+    # todo: post["date"] = getDate()
+    return post
+
+
+def create_thread(thread_name, first_post):
+    posts = [first_post]
+    threads[thread_name] = posts
+
+
+def add_reply(thread_name, post):
+    thread = threads[thread_name]
+    thread.append(post)
+
+
+def validate(email, password):
+    if email not in users.keys():
+        print("No such email registered")
+        return False
+    return users[email]["password"] == password
+
+
+def get_threads():
+    return threads
+
+
+def get_thread(thread_name):
+    return threads[thread_name]
+
+
+def has_thread(thread_name):
+    return thread_name in threads.keys()
+
+
+def get_users():
+    return users
+
+
+def get_user(email):
+    return users[email]
--- server.py
+++ server.py
@@ -3,46 +3,17 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+
+from forum.models import create_user, create_post, create_thread, add_reply, validate, get_threads, get_thread, \
+    get_user, has_thread
+
 app = Flask(__name__)
 
-threads = {}
-users = {}
-
-def create_user(username, email, password):
-    if email in users.keys():
-        print(email + " has already been registered")
-        return
-    user = {}
-    user["username"] = username
-    user["email"] = email
-    user["password"] = password
-    users[email] = user
-
-def create_post(poster, contents):
-    post = {}
-    post["poster"] = poster
-    # todo: post["date"] = getDate()
-    post["content"] = contents
-    return post
-
-def create_thread(threadname, firstpost):
-    posts = []
-    posts.append(firstpost)
-    threads[threadname] = posts
-
-def add_reply(threadname, post):
-    thread = threads[threadname]
-    thread.append(post)
-
-def validate(email, password):
-    if email not in users.keys():
-        print("No such email registered")
-        return False
-    return users[email]["password"] == password
 
 @app.route("/")
 def index():
-    return render_template("index.html", threads=threads.keys())
+    return render_template("index.html", threads=get_threads().keys())
+
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
@@ -56,8 +27,9 @@ def register():
         create_user(request.form['name'], request.form['email'], request.form['pwd'])
         return render_template("welcome.html", username=request.form['name'])
 
+
 @app.route("/makethread", methods=["GET", "POST"])
-def makeThread():
+def make_thread():
     if request.method == "GET":
         return render_template("makethread.html")
     else:
@@ -65,17 +37,18 @@ def makeThread():
         password = request.form['pwd']
         if not validate(email, password):
             return "Invalid user!"
-        threadname = request.form['threadname']
+        thread_name = request.form['threadname']
         post_contents = request.form['firstpost']
-        post = create_post(users[email]['username'], post_contents)
-        create_thread(threadname, post)
-        return render_template("thread.html", title=threadname, posts=threads[threadname])
+        post = create_post(get_user(email)['username'], post_contents)
+        create_thread(thread_name, post)
+        return render_template("thread.html", title=thread_name, posts=get_thread(thread_name))
 
-@app.route("/thread/<threadname>", methods=["GET","POST"])
-def getThread(threadname):
+
+@app.route("/thread/<thread_name>", methods=["GET", "POST"])
+def view_thread(thread_name):
     if request.method == "GET":
-        if threadname in threads:
-            return render_template("thread.html", title=threadname, posts=threads[threadname])
+        if has_thread(thread_name):
+            return render_template("thread.html", title=thread_name, posts=get_thread(thread_name))
         return "<h1>Invalid thread!</h1>"
     else:
         email = request.form['email']
@@ -84,5 +57,9 @@ def getThread(threadname):
             return "Invalid user!"
         post_contents = request.form['post']
         post = create_post(email, post_contents)
-        add_reply(threadname, post)
-        return render_template("thread.html", title=threadname, posts=threads[threadname])
+        add_reply(thread_name, post)
+        return render_template("thread.html", title=thread_name, posts=get_thread(thread_name))
+
+
+if __name__ == "__main__":
+    app.run(ssl_context='adhoc')
